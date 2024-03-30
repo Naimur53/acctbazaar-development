5804a4edb456043cb91f107c65a62c4725cb4555
naimur53 <naimurrhman53@gmail.com>
2024-03-30 07:28:21 +0600

    deon

--- src/components/dashboard/DashboardSalesDynamics.tsx
+++ src/components/dashboard/DashboardSalesDynamics.tsx
@@ -2,44 +2,59 @@ import Image from "next/image";
 import { useState } from "react";
 import DashboardCard from "./DashboardCard";
 import AppTabs from "../ui/AppTabs";
-import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts";
 import { Tooltip } from "antd";
 import { useGetSellerOverviewQuery } from "@/redux/features/user/userApi";
 import { customizeData } from "@/utils/sellerSaleDataCustomize";
 import AppRenderReduxData from "../ui/AppRenderReduxData";
 
 export default function DashboardSalesDynamics() {
-    const queryInfo = useGetSellerOverviewQuery("");
+  const queryInfo = useGetSellerOverviewQuery("");
 
-    return (
-        <div className='w-full rounded-lg shadow-md pt-4 pb-12 px-5 border border-[#FAFAFA] '>
-            <div className='text-textBlueBlack flex items-center justify-between pb-6'>
-                <h2 className="text-xl">Sales Dynamics</h2>
-                <h2 className="text-sm">2024</h2>
-            </div>
+  return (
+    <div className="w-full rounded-lg shadow-md pt-4 pb-12 px-5 border border-[#FAFAFA] ">
+      <div className="text-textBlueBlack flex items-center justify-between pb-6">
+        <h2 className="text-xl">Sales Dynamics</h2>
+        <h2 className="text-sm">2024</h2>
+      </div>
 
-            {/* <AppTabs tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} /> */}
+      {/* <AppTabs tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} /> */}
 
-            <div className=''>
-                <AppRenderReduxData
-                    isEmptyComponentHave
-                    queryData={queryInfo}
-                    showData={(data) => {
-                        const chartData = customizeData(data?.data.pastYearData);
-                        return (
-                            // <ResponsiveContainer width="100%" height="100%">
-                            <BarChart barSize={10} width={550} height={250} data={chartData}>
-                                <CartesianGrid strokeDasharray="3 3" />
-                                <XAxis dataKey="month" />
-                                <YAxis dataKey="totalSold" />
-                                <Tooltip />
-                                <Bar dataKey="totalSold" fill="#FF5A35" />
-                            </BarChart>
-                            // </ResponsiveContainer>
-                        )
-                    }}
-                />
-            </div>
-        </div>
-    );
-};
+      <div className="">
+        <AppRenderReduxData
+          isEmptyComponentHave
+          queryData={queryInfo}
+          showData={(data) => {
+            const chartData = customizeData(data?.data.pastYearData);
+            return (
+              <div className=" h-[300px]">
+                <ResponsiveContainer width="100%" height="100%">
+                  <BarChart
+                    barSize={10}
+                    width={550}
+                    height={250}
+                    data={chartData}
+                  >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis dataKey="totalSold" />
+                    <Tooltip />
+                    <Bar dataKey="totalSold" fill="#FF5A35" />
+                  </BarChart>
+                </ResponsiveContainer>
+              </div>
+            );
+          }}
+        />
+      </div>
+    </div>
+  );
+}
--- src/pages/seller/dashboard.tsx
+++ src/pages/seller/dashboard.tsx
@@ -4,6 +4,7 @@ import DashboardSalesDynamics from "@/components/dashboard/DashboardSalesDynamic
 import AppDatePicker from "@/components/ui/AppDatePicker";
 import HomeLayout from "@/layout/HomeLayout";
 import PrivateLayout from "@/layout/PrivateLayout";
+import SellerLayout from "@/layout/SellerLayout";
 import { useGetSellerOverviewQuery } from "@/redux/features/user/userApi";
 import { useState } from "react";
 import { LiaUserCheckSolid } from "react-icons/lia";
@@ -11,81 +12,80 @@ import { RiCoinsLine } from "react-icons/ri";
 import { TbUserCircle, TbUserDollar } from "react-icons/tb";
 
 export default function Dashboard() {
+  const [date, setDate] = useState<any>(null);
+  const { data } = useGetSellerOverviewQuery("");
 
-    const [date, setDate] = useState<any>(null);
-    const { data } = useGetSellerOverviewQuery("")
+  const dashboardCards = [
+    {
+      title: "Total amount",
+      icon: <RiCoinsLine className="text-xl text-textGrey" />,
+      amount: data?.data?.totalMoney,
+      // rate: 10,
+      // increase: false,
+      // timeRange: "month"
+    },
+    {
+      title: "Total accounts",
+      icon: <TbUserCircle className="text-xl text-textGrey" />,
+      amount: data?.data?.totalAccount,
+      // rate: 40,
+      // increase: true,
+      // timeRange: "month"
+    },
+    {
+      title: "Accounts sold",
+      icon: <TbUserDollar className="text-xl text-textGrey" />,
+      amount: data?.data?.totalSoldAccount,
+      // rate: 40,
+      // increase: true,
+      // timeRange: "month"
+    },
+    {
+      title: "Accounts approved",
+      icon: <LiaUserCheckSolid className="text-xl text-textGrey" />,
+      amount: data?.data?.totalAccountApprove,
+      // rate: 40,
+      // increase: true,
+      // timeRange: "month"
+    },
+  ];
 
-    const dashboardCards = [
-        {
-            title: "Total amount",
-            icon: <RiCoinsLine className="text-xl text-textGrey" />,
-            amount: data?.data?.totalMoney,
-            // rate: 10,
-            // increase: false,
-            // timeRange: "month"
-        },
-        {
-            title: "Total accounts",
-            icon: <TbUserCircle className="text-xl text-textGrey" />,
-            amount: data?.data?.totalAccount,
-            // rate: 40,
-            // increase: true,
-            // timeRange: "month"
-        },
-        {
-            title: "Accounts sold",
-            icon: <TbUserDollar className="text-xl text-textGrey" />,
-            amount: data?.data?.totalSoldAccount,
-            // rate: 40,
-            // increase: true,
-            // timeRange: "month"
-        },
-        {
-            title: "Accounts approved",
-            icon: <LiaUserCheckSolid className="text-xl text-textGrey" />,
-            amount: data?.data?.totalAccountApprove,
-            // rate: 40,
-            // increase: true,
-            // timeRange: "month"
-        },
-    ];
-
-    return (
-        <HomeLayout>
-            <PrivateLayout>
-                <div className='container py-5 md:py-10 2xl:py-12'>
-                    {/* this is top section div  */}
-                    <div className='flex flex-col md:flex-row gap-1 justify-between'>
-                        <div className=''>
-                            <h2 className="title">My Dashboard</h2>
-                            <p className="text-textGrey text-xs md:text-sm">Access all products on the marketplace by our verified sellers</p>
-                        </div>
-                        {/* <AppDatePicker
+  return (
+    <HomeLayout>
+      <SellerLayout>
+        <div className="container py-5 md:py-10 2xl:py-12">
+          {/* this is top section div  */}
+          <div className="flex flex-col md:flex-row gap-1 justify-between">
+            <div className="">
+              <h2 className="title">My Dashboard</h2>
+              <p className="text-textGrey text-xs md:text-sm">
+                Access all products on the marketplace by our verified sellers
+              </p>
+            </div>
+            {/* <AppDatePicker
                             placeholder="Select date"
                             setDate={setDate}
                         /> */}
-                    </div>
+          </div>
 
-                    {/* this is main div  */}
-                    <div className='w-full space-y-2 md:space-y-6 bg-white md:p-5 2xl:p-6 mt-2 md:mt-4 lg:mt-5 2xl:mt-6'>
-                        <div className='grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-5 2xl:gap-6'>
-                            {
-                                dashboardCards.map(card => (
-                                    <DashboardCard key={card?.title} card={card} />
-                                ))
-                            }
-                        </div>
+          {/* this is main div  */}
+          <div className="w-full space-y-2 md:space-y-6 bg-white md:p-5 2xl:p-6 mt-2 md:mt-4 lg:mt-5 2xl:mt-6">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-5 2xl:gap-6">
+              {dashboardCards.map((card) => (
+                <DashboardCard key={card?.title} card={card} />
+              ))}
+            </div>
 
-                        <div className='grid grid-cols-1 md:grid-cols-2 gap-5 2xl:gap-6'>
-                            {/* this is left side dev  */}
-                            <DashboardPayments />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 2xl:gap-6">
+              {/* this is left side dev  */}
+              <DashboardPayments />
 
-                            {/* this is right side dev  */}
-                            <DashboardSalesDynamics />
-                        </div>
-                    </div>
-                </div>
-            </PrivateLayout>
-        </HomeLayout>
-    );
-};
+              {/* this is right side dev  */}
+              <DashboardSalesDynamics />
+            </div>
+          </div>
+        </div>
+      </SellerLayout>
+    </HomeLayout>
+  );
+}
